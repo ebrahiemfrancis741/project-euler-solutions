2ea6465ac38ca4aca35f48b58c0cb29c44a8db16
Ebrahiem Francis <ebrahiemfrancis741@gmail.com>
2025-01-28 18:17:07 +0200

    implement sum(arr) function that sums all even numbers in sequence

--- p2.js
+++ p2.js
@@ -1,13 +1,31 @@
 // problem 2 https://projecteuler.net/problem=2
 
-function fibonacci(n){
-  if(n == 0){
-    return 0;
-  }
-  else if(n == 1){
-    return 1;
-  }
-  else{
-    return fibonacci(n - 1) + fibonacci(n - 2);
+
+// generate sequence
+function fibonacci(n) {
+  if (n == 0) {
+    return [0];
+  } else if (n == 1) {
+    return [0, 1];
+  } else {
+    let sequence = [0, 1];
+    for (let i = 2; i <= n; i++) {
+      sequence.push(sequence[i - 1] + sequence[i - 2]);
+    }
+    return sequence;
   }
 }
+
+// sum even values in the sequence
+function sum(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 == 0) {
+      sum += arr[i]
+    }
+  }
+  return sum;
+}
+
+// 33rd number in sequence is the highest number less than 4 million so we use that
+console.log(sum(fibonacci(33)));
