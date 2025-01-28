1bb33c1b5b392b2e83529a15e0dfb2b04efa07bd
Ebrahiem Francis <ebrahiemfrancis741@gmail.com>
2025-01-28 16:43:40 +0200

    Start problem 2, implement fibonacci(n), which calculates nth fibonacci number

--- p1.js
+++ p1.js
@@ -1,46 +1,4 @@
-/*
-  for all inputs n >= 1
-  n = 1
-    no multiples of 3 or 5
-    sum of multiples = 0
-    return 0
-  n = 2
-    no multiples of 3 or 5
-    sum of multiples = 0
-    return 0
-  n = 3
-    no multiples of 3 or 5
-    sum of multiples = 0
-    return 0
-  n = 4
-    multiples = 3
-    sum of multiples = 3
-    return 3
-  n = 5
-    multiples = 3
-    sum of multiples = 3
-    return 3
-  n = 6
-    multiples = 3,5
-    sum of multiples = 8
-    return 8
-  n = 7
-    multiples = 3,5,6
-    sum of multiples = 14
-    return 14
-  n = 8
-    multiples = 3,5,6
-    sum of multiples = 14
-    return 14
-  n = 9
-    multiples = 3,5,6
-    sum of multiples = 14
-    return 14
-  n = 10
-    multiples = 3,5,6,9
-    sum of multiples = 23
-    return 23
-*/
+//problem 1 https://projecteuler.net/problem=1
 
 /*
   Find the multiples of 3 and 5 of numbers lower than n recursively
