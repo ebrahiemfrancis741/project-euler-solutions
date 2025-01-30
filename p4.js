0dc2b9e4515f4181c3ab10c452aacd2b7aea61aa
Ebrahiem Francis <ebrahiemfrancis741@gmail.com>
2025-01-30 18:43:52 +0200

    Update getLargestPalindromeProduct() to add all palindromes to a list and find the largest

--- p4.js
+++ p4.js
@@ -14,7 +14,13 @@ If we still did no find the number, start decreasing a by 1, then
 repeat the process of decreasing b all the way to 100 and so on 
 until we maybe get the number...
 
-lets first implement a function that checks if a number is palindromic
+lets first implement a function that checks if a number is palindromic.
+Then lets try to see if our algorithm will even work, it seems like 
+the time complexity for this will be like n^2 or worse Im not sure :).
+
+It seems our way didnt work, I think i know what to do. My idea is to store 
+all palidromic products, in a list, after all palindromic products have 
+been found. Then find the largest in the list. lets try
 */
 
 function isPalindromic(n) {
@@ -29,3 +35,48 @@ function isPalindromic(n) {
   }
 }
 
+/*
+Attempt #1 - failed
+function getLargestPalindromeProduct() {
+  let a = 999;
+  let b = 999;
+  let product;
+  for (let i = a; i >= 100; i--) {
+    for (let j = b; j >= 100; j--) {
+      product = i * j;
+      if (isPalindromic(product)) {
+        console.log(`i * j = ${i} * ${j} = ${i * j}`);
+        return product;
+      }
+    }
+  }
+}
+*/
+
+function getLargestPalindromeProduct() {
+  let a = 999;
+  let b = 999;
+  let product;
+  let palidromes = [];
+  // get a list of palindromic products
+  for (let i = a; i >= 100; i--) {
+    for (let j = b; j >= 100; j--) {
+      product = i * j;
+      if (isPalindromic(product)) {
+        console.log(`i * j = ${i} * ${j} = ${i * j}`);
+        palidromes.push(product);
+      }
+    }
+  }
+
+  // find largest palindrome
+  let largest = palidromes[0];
+  for (let i = 1; i < palidromes.length; i++) {
+    if (palidromes[i] > largest) {
+      largest = palidromes[i];
+    }
+  }
+  return largest;
+}
+
+console.log(getLargestPalindromeProduct());
