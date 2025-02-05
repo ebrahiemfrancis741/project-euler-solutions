8326c42e33e95aee33de0daa762177d9ade19846
Ebrahiem Francis <ebrahiemfrancis741@gmail.com>
2025-02-05 06:53:15 +0200

    Solve problem 6

---
+++ p6.js
@@ -0,0 +1,20 @@
+function sumOfSquares(n) {
+  let sum = 0;
+  let power;
+  for (let i = 1; i <= n; i++) {
+    power = Math.pow(i, 2);
+    sum += power;
+  }
+  return sum;
+}
+
+function squareOfSum(n) {
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += i;
+  }
+
+  return Math.pow(sum, 2);
+}
+
+console.log(squareOfSum(100) - sumOfSquares(100));
